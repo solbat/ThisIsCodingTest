2c50018f6d4eee09566d569fd203de1d5939a406
solbat <alswnd6397@gmail.com>
2022-04-27 22:18:49 +0900

    220427 : ch10 example finished

--- ch10_graph_theory/10-4.py
+++ ch10_graph_theory/10-4.py
@@ -1,4 +1,4 @@
-# 10-3.py 개선된 서로소 집합 알고리즘 소스코드
+# 10-4.py 서로소 집합을 활용한 사이클 판별 소스코드
 
 # 특정 원소가 속한 집합을 찾기
 def find_parent(parent, x):
@@ -11,7 +11,7 @@ def find_parent(parent, x):
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a < b: 
+    if a < b:
         parent[b] = a
     else:
         parent[a] = b
@@ -24,22 +24,19 @@ parent = [0] * (v+1) # 부모 테이블 초기화
 for i in range(1, v+1):
     parent[i] = i
 
-# union 연산을 각각 수행
+cycle = False # 사이클 발생 여부
+
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
-    for i in range(1, v+1):
-        print(parent[i], end=' ')
-    print()
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(union) 수행
+    else:
+        union_parent(parent, a, b)
 
-# 각 원소가 속한 집합 출력
-print('각 원소가 속한 집합: ', end='')
-for i in range(1, v+1):
-    print(find_parent(parent, i), end=' ')
-
-print()
-
-# 부모 테이블 내용 출력
-print('부모 테이블: ', end='')
-for i in range(1, v+1):
-    print(parent[i], end=' ')
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
